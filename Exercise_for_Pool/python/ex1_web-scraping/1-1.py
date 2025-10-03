6c7f0d126491e85ebf796506b27c5dbf2713cc29
Sakihama Reina <lizhuqibin@gmail.com>
2025-10-03 18:03:24 +0900

    Add files via upload

--- Exercise_for_Pool/python/ex1_web-scraping/1-1.py
+++ Exercise_for_Pool/python/ex1_web-scraping/1-1.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-get_ipython().system('pip install bs4')
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -12,36 +5,19 @@ import pandas as pd
 import time
 
 
-# In[75]:
-
-
 url="https://r.gnavi.co.jp/area/jp/izakaya/rs/"
 next_url="https://r.gnavi.co.jp/area/jp/izakaya/rs/?p=2"
 column=["店舗名","電話番号","メールアドレス","都道府県","市区町村","番地","建物名","URL","SSL"]
-stores_list=get_list(url)
 
-if len(stores_list)<50:
-    time.sleep(5)
-    stores_list.extend(get_list(next_url))
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0"
+}
 
-
-# In[82]:
-
-
-data=[]
-for store in stores_list:
-    data.append(store) if len(data)<50 else ""
-
-df = pd.DataFrame(data,columns=column)
-
-df.to_csv("sample.csv", index=False, encoding="utf-8-sig")
-
-
-# In[74]:
-
+response = requests.get(url,headers=headers)
+soup = BeautifulSoup(response.text, "html.parser")
 
 def get_list(url):
-    response=requests.get(url)
+    response=requests.get(url,headers=headers)
     soup = BeautifulSoup(response.text,"html.parser")
     content = soup.find_all("a",class_="style_titleLink___TtTO")
     link=[]
@@ -51,40 +27,54 @@ def get_list(url):
 
     stores_list=[]
     for link_url in link:
+        time.sleep(3)
         storelist=[]
-        res=requests.get(link_url)
+        res=requests.get(link_url,headers=headers)
         res.encoding = res.apparent_encoding
         soup=BeautifulSoup(res.text,"html.parser")
         name=soup.find("h1",class_="shop-info__name").text.strip().replace('\xa0', '')
         number=soup.find("span",class_="number").text
-        mail=" "
+        mail=get_mail(link_url)
         region=soup.find("span",class_="region").text
-        m=region_se(region)
-        storelist+=[name,number,mail,m.group(1),m.group(2),f"'{m.group(3)}",m.group(4) if m.group(4) else" ",link_url,ssl(link_url)]
+        pref,city,ban,building=region_se(region)
+        storelist+=[name,number,mail,pref,city,f"'{ban}",building,"" ,""]
         stores_list.append(storelist)
     return stores_list
 
 
-# In[70]:
-
+def get_mail(url):
+    time.sleep(3)
+    mail_response=requests.get(url,headers=headers)
+    mail_soup=BeautifulSoup(mail_response.text,"html.parser")
+    text=mail_soup.get_text()
+    mail_match = re.search(r"[\w\.-]+@[\w\.-]+",text)
+    if mail_match:
+        return mail_match.group()
+    return ""
 
 def region_se(region):
-    pattern="(...??[都道府県])(.+?)(\d+(?:-\d+)*)([^\d\-].*)?"
+    time.sleep(3)
+    pattern=r"(.*?[都道府県])(.+?[市区町村])(.+?)(?=\d)([\d\-]+)?(.*)"
     m=re.match(pattern,region)
-    return m
+    if m:
+        pref = m.group(1)
+        city = m.group(2)
+        ban = m.group(3)+m.group(4)
+        building = m.group(5) or ""
+        return pref,city,ban,building
+    else:
+        return "","","",""
 
+stores_list=get_list(url)
 
-# In[12]:
+if len(stores_list)<50:
+    time.sleep(5)
+    stores_list.extend(get_list(next_url))
 
+data=[]
+for store in stores_list:
+    data.append(store) if len(data)<50 else ""
 
-import re
-def ssl(shop_link):
-    for tag in shop_link:
-                
-        if re.match(r'^https?://', url):   
-            if url.startswith("https://"):
-                 return("True")
-            else:
-                return("False")
-            
+df = pd.DataFrame(data,columns=column)
 
+df.to_csv("1-1.csv", index=False, encoding="utf-8-sig")
